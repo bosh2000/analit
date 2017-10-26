5a88e4b8dff95175b4d4fcd5c1bd3eedbb91e4c8
Glupak <Glupak@GLUPAK>
2017-10-26 10:19:36 +1000

    Add LoginWindows

---
+++ Analit/LoginWindows.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Analit
+{
+	/// <summary>
+	/// Interaction logic for LoginWindows.xaml
+	/// </summary>
+	public partial class LoginWindows : Window
+	{
+		public LoginWindows()
+		{
+			InitializeComponent();
+		}
+
+		private void okButton_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow mWnd = new MainWindow();
+			mWnd.Show();
+			this.Close();
+		}
+
+		
+	}
+}
